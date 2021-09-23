81fb655ae3765ab490fa7597bb481c988b1bd09f
Christian Strydom <christian.strydom@gmail.com>
2021-09-23 15:39:56 +0200

    Added button to access pixelrun.co.za/dev

--- TitleScreen/TitleScreen.gd
+++ TitleScreen/TitleScreen.gd
@@ -5,7 +5,3 @@ func _ready():
 		Global.high_score = Global.score_time
 		
 	$High_Score.text = "High Score: " + str(Global.high_score)
-	
-func _process(delta):
-	if Input.is_action_just_pressed("jump"):
-		get_tree().change_scene("res://Level/Level.tscn")
